6469af1216cbb84df850525f0cb5f4ff3d5d0bb2
Jannis R <mail@jannisr.de>
2022-09-24 18:51:17 +0200

    enforce & document EPC QR code limitations 📝

--- index.js
+++ index.js
@@ -23,8 +23,16 @@ const generateQrCode = data => {
 	data = Object.assign(Object.create(null), defaults, data)
 
 	assertNonEmptyString(data.name, 'data.name')
-	if ('bic' in data) assertNonEmptyString(data.bic, 'data.bic') // todo: validate
+	if (data.name.length > 70) throw new Error('data.name must have <=70 characters')
 
+	// > AT-23 BIC of the Beneficiary Bank
+	if ('bic' in data) {
+		assertNonEmptyString(data.bic, 'data.bic')
+		if (data.bic.length > 11) throw new Error('data.bic must have <=11 characters')
+		// todo: validate more?
+	}
+
+	// > AT-04 Amount of the Credit Transfer in Euro
 	if ('number' !== typeof data.amount) throw new Error('data.amount must be a number.')
 	if (data.amount < 0.01 || data.amount > 999999999.99) {
 		throw new Error('data.amount must be >=0.01 and <=999999999.99.')
@@ -33,24 +41,33 @@ const generateQrCode = data => {
 		throw new Error('data.currency must be a valid currency code.')
 	}
 
+	// > AT-44 Purpose of the Credit Transfer
 	if ('purposeCode' in data) {
 		assertNonEmptyString(data.purposeCode, 'data.purposeCode')
+		if (data.purposeCode.length > 4) throw new Error('data.purposeCode must have <=4 characters')
 		// todo: validate against AT-44
 	}
+
+	// > AT-05 Remittance Information (Structured)
+	// > Creditor Reference (ISO 11649 RF Creditor Reference may be used)
 	if ('structuredReference' in data) {
 		assertNonEmptyString(data.structuredReference, 'data.structuredReference')
-		// todo: validate against AT-05, validate <=35 chars
+		if (data.structuredReference.length > 35) throw new Error('data.structuredReference must have <=35 characters')
+		// todo: validate against AT-05
 	}
+	// > AT-05 Remittance Information (Unstructured)
 	if ('unstructuredReference' in data) {
 		assertNonEmptyString(data.unstructuredReference, 'data.unstructuredReference')
-		// todo: validate <=140 chars
+		if (data.unstructuredReference.length > 140) throw new Error('data.unstructuredReference must have <=140 characters')
 	}
 	if (('structuredReference' in data) && ('unstructuredReference' in data)) {
 		throw new Error('Use either data.structuredReference or data.unstructuredReference.')
 	}
+
+	// > Beneficiary to originator information
 	if ('information' in data) {
 		assertNonEmptyString(data.information, 'data.information')
-		// todo: validate <=70 chars
+		if (data.information.length > 70) throw new Error('data.information must have <=70 characters')
 	}
 
 	return [
